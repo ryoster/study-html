72ec82769d75ab98d70296c5a3719d05991360dc
ryoster <ryoster0809@gmail.com>
2017-05-26 14:02:27 +0900

    first commit

---
+++ doc/assets/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    // default
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+    // add
+    $(".dropdown-button").dropdown();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
